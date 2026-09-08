554ce8c2ddba51437b6789edf2abea999fcb83d0
nikhil19tiwari <nikhil19tiwari@gmail.com>
2026-09-08 23:02:53 +0530

    adding files

--- sarvan_kirana_store/vite.config.js
+++ sarvan_kirana_store/vite.config.js
@@ -1,8 +1,37 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react() , tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+
+      manifest: {
+        name: 'Sarvan Kirana Store',
+        short_name: 'Sarvan Store',
+        description: 'Sarvan Kirana Store Online',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/',
+        scope: '/',
+        icons: [
+          {
+            src: '/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
 })
